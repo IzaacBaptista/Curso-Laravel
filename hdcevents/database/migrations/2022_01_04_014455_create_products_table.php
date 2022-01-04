87854d359514be041f332395ef54f57e57900563
Izaac Baptista <izaacbaptista@gmail.com>
2022-01-03 22:54:19 -0300

    migrations

--- hdcevents/database/migrations/2022_01_04_014455_create_products_table.php
+++ hdcevents/database/migrations/2022_01_04_014455_create_products_table.php
@@ -15,7 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
+            $table->integer('qtd');
+            $table->text('description');
             $table->timestamps();
         });
     }
