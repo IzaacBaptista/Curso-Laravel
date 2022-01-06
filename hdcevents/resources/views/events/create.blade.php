7deaa82532c3a334a6d03b631497fa8586160338
Izaac Baptista <izaacbaptista@gmail.com>
2022-01-06 18:40:03 -0300

    Salvando dados no banco MySQL

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-    <h1>Crie um evento</h1>
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control">
+        </div>
+    </form>
+</div>
+
 @endsection
