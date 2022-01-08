ad095463907ea00a36e32e4f44d533f2eb17048d
Izaac Baptista <izaacbaptista@gmail.com>
2022-01-08 07:40:13 -0300

    Resgatando um registro do banco de dados

--- hdcevents/resources/views/events/show.blade.php
+++ hdcevents/resources/views/events/show.blade.php
@@ -4,5 +4,18 @@
 
 @section('content')
 
+    <div class="col-md-10 offset-md-1">
+        <div class="row">
+            <div id="image-container" class="col-md-6">
+                <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}" class="img-fluid">
+            </div>
+            <div id="info-container" class="col-md-6">
+                <h1>{{ $event->title }}</h1>
+                <p class="event-city">
+                    <ion-icon name="location-outline"></ion-icon> {{ $event->city }}
+                </p>
+            </div>
+        </div>
+    </div>
 
 @endsection
