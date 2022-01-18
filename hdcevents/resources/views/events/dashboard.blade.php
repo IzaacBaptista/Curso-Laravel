cd5bf3fddc53a1836b34980a0b69d67e64f21d36
Izaac Baptista <izaacbaptista@gmail.com>
2022-01-18 11:51:59 -0300

    Criando a dashboard

--- hdcevents/resources/views/events/dashboard.blade.php
+++ hdcevents/resources/views/events/dashboard.blade.php
@@ -9,8 +9,28 @@
 </div>
 <div class="col-md-10 offset-md-1 dashboard-events-container">
     @if(count(events) > 0)
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Participantes</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($events as $event)
+                    <tr>
+                        <th scope="row">{{ $loop->index + 1 }}</th>
+                        <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @else
-    <p>Você ainda não tem eventos, <a href="/events/create">criar evento</a></p>
+        <p>
+            Você ainda não tem eventos, <a href="/events/create">criar evento</a>
+        </p>
     @endif
 </div>
 
